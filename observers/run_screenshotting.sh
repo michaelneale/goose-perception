4820a63f3401a508ccd56c382c45d70ab053ba31
Michael Neale <michael.neale@gmail.com>
2025-07-14 15:52:10 +1000

    checkpoint

--- observers/run_screenshotting.sh
+++ observers/run_screenshotting.sh
@@ -3,6 +3,30 @@
 # Sleep for 5 seconds
 sleep 5
 
+# Enumerate open windows and their titles
+echo "=== Open Windows ==="
+osascript -e '
+tell application "System Events"
+    set windowList to {}
+    repeat with proc in application processes
+        try
+            if (count of windows of proc) > 0 and visible of proc is true then
+                set appName to name of proc
+                repeat with win in windows of proc
+                    try
+                        set winTitle to name of win
+                        set end of windowList to (appName & ": " & winTitle)
+                    end try
+                end repeat
+            end if
+        end try
+    end repeat
+    return windowList
+end tell
+' 2>/dev/null
+
+echo "===================="
+
 # Take screenshots of all displays (silent)
 screencapture -x -D 1 ./screen1.png 2>/dev/null || true
 screencapture -x -D 2 ./screen2.png 2>/dev/null || true
